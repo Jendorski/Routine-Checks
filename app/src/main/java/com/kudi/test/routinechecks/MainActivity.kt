90330f08bc9ea4254e64410de46384a8cb537c14
Jendorski <gabjide@gmail.com>
2019-11-06 10:18:11 +0100

    Initial commit

---
+++ app/src/main/java/com/kudi/test/routinechecks/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kudi.test.routinechecks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
